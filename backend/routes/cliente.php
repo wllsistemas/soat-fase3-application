3cf6465f736437c33f0bb182c9b48c9a93daf887
WILLIAM FRANCISCO LEITE <william@wllsistemas.com.br>
2026-01-08 06:59:37 -0300

    feat: adiciona validação de cpf na rota red one

--- backend/routes/cliente.php
+++ backend/routes/cliente.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/cliente', [ClienteApi::class, 'create']);
 
 Route::get('/cliente', [ClienteApi::class, 'read']);
-Route::get('/cliente/{uuid}', [ClienteApi::class, 'readOne']);
+Route::get('/cliente/{uuid}', [ClienteApi::class, 'readOne'])->middleware(DocumentoObrigatorioMiddleware::class);
 
 Route::put('/cliente/{uuid}', [ClienteApi::class, 'update']);
 Route::delete('/cliente/{uuid}', [ClienteApi::class, 'delete']);
